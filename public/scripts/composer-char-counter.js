dd47811c8775878135997f3807331c616b3c3f0b
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-07-13 17:59:37 -0400

    Creating the tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-    $("#tweet-text").keyup(function() {
-        $(".counter").html(140 - this.value.length);
-        if (this.value.length > 140) {
-            $(".counter").addClass("counterNegative").removeClass("counterPositive");
-        } else {
-            $(".counter").removeClass("counterNegative").addClass("counterPositive");
-        }
-    });
+  $("#new-tweet-text").keyup(function() {
+    $(".counter").html(140 - this.value.length);
+    if (this.value.length > 140) {
+      $(".counter").addClass("counterNegative").removeClass("counterPositive");
+      $(".counter").parent().children("button").attr("disabled", "disabled");
+    } else {
+      $(".counter").removeClass("counterNegative").addClass("counterPositive");
+      $(".counter").parent().children("button").removeAttr("disabled");
+    }
+  });
 
 });
