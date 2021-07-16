bbe94c400858555377eab072be08f57fdafbe789
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-07-16 12:22:27 -0400

    Fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,29 +5,29 @@
  */
 
 // Fake data taken from initial-tweets.json
-const data = [{
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [{
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 const createTweetElement = function(tweet) {
 
@@ -42,12 +42,17 @@ const renderTweets = function(tweets) {
   }
 };
 
+const loadtweets = function() {
+  $.getJSON('/tweets', function(tweetsdata) {
+    let sortedTweets = tweetsdata.sort((a, b) => a.created_at > b.created_at && -1 || 1);
+    renderTweets(sortedTweets);
+  });
+};
 
 $(document).ready(function() {
-  renderTweets(data);
+  loadtweets();
   $(".new-tweet-form").submit(function(event) {
     event.preventDefault();
-
     $.post("/tweets", $(".new-tweet-form").serialize());
     window.location.reload();
   });
